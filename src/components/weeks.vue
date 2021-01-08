9b83da73f427520fc448d1e00ca3e3a0f5869b5d
Andrea <andrea-dibra@hotmail.com>
2021-01-08 22:15:36 +0100

    Adding weeks componetnt

---
+++ src/components/weeks.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+            <aside>
+        <div class="sideBarContainer">
+                <ul>
+                    <li>Monday</li>
+                    <li>Tuesday</li>
+                    <li>Wednesday</li>
+                    <li>Tursday</li>
+                    <li>Friday</li>
+                </ul>
+        </div>
+            </aside>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+aside{
+    float :left;
+    padding: 5px 15px;
+    height: 98.5vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+li{
+    list-style-type: none;
+    padding: 5px;
+}
+
+</style>
