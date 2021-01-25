9800e6a5a77039fc5af4b6880910c77fb6d897f8
Andrea <andrea-dibra@hotmail.com>
2021-01-25 20:17:33 +0100

    ....

--- app/src/main.js
+++ app/src/main.js
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
 import store from './state/store'
+import axios from 'axios'
 
 Vue.use(Vuex);
+vue.ues(axios);
 
 Vue.config.productionTip = false
 
