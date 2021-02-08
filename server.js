033b8a3ff21524b29ae37b3e2b370e78e2415e6e
Andrea <andrea-dibra@hotmail.com>
2021-02-08 21:35:12 +0100

    removing history

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-var history = require('connect-history-api-fallback');
-
 const app = express();
 
-
-var hist = history({
-    rewrites: [
-        { from: /\*/, to: '/index.html' }
-    ]
-});
-
-
-app.use(hist)
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
-
 
 const port = process.env.PORT || 8080
 
