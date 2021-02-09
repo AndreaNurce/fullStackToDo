bbfbaae7d3ec910ff010e0dd27c954ccadfbc008
Andrea <andrea-dibra@hotmail.com>
2021-02-09 19:49:35 +0100

    server changes

--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const app = express();
 
+
+
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 const port = process.env.PORT || 8080
 
 app.listen(port)
