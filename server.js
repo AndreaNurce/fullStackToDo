f4631c2f66d0746da5f9cd002fd4a41afea57191
Andrea <andrea-dibra@hotmail.com>
2021-02-09 19:59:49 +0100

    adding dist

--- server.js
+++ server.js
@@ -13,8 +13,17 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 }
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
+
+app.use(function (req, res, next) {
+    res.setHeader(
+        'Content-Security-Policy',
+        "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+    );
+    next();
+});
+
 const port = process.env.PORT || 8080
 
 app.listen(port)
