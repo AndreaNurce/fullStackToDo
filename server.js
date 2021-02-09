6b5882d46e822838f0408892770ae66597f514dd
Andrea <andrea-dibra@hotmail.com>
2021-02-09 19:55:10 +0100

    ..

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
+const cors = require('cors')
 const app = express();
 
 
-
+app.use(cors())
 app.use('/',serveStatic(path.join(__dirname,'/dist')))
 
 if (process.env.NODE_ENV === 'production') {
