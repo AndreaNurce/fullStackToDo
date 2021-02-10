4f3c4ab41fb9f40f3907b355743d743961ff7e76
Andrea <andrea-dibra@hotmail.com>
2021-02-10 20:45:44 +0100

    setting back server

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const app = express();
+const cors = require('cors')
 
-app.use('*',serveStatic(path.join(__dirname,'/dist')))
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+}
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'dist', 'index.html'));
+});
 
 app.use(function (req, res, next) {
     res.setHeader(
