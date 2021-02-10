0411fe21e47ad5951b6c9f3fca9583b1fd733001
Andrea <andrea-dibra@hotmail.com>
2021-02-10 20:41:42 +0100

    fixing server.js

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const cors = require('cors')
 const app = express();
 
-
-app.use(cors())
-app.use('/',serveStatic(path.join(__dirname,'/dist')))
-
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'));
-}
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'dist', 'index.html'));
-});
+app.use('*',serveStatic(path.join(__dirname,'/dist')))
 
 app.use(function (req, res, next) {
     res.setHeader(
