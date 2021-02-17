23c3a6ffe8916c5b633f5998a6ce9084db5e20a1
Andrea <andrea-dibra@hotmail.com>
2021-02-17 10:32:49 +0100

    / fix

--- src/state/store.js
+++ src/state/store.js
@@ -13,7 +13,7 @@ export default new Vuex.Store({
         important: false,
         toDoText : '',
         response : [],
-        url: 'https://back-en-to-do.herokuapp.com//',
+        url: 'https://back-en-to-do.herokuapp.com/',
     },
     mutations :{
         async getDate(state) {
